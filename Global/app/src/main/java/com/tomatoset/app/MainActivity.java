4515d9dfb000eb806e82464ebcaf1c4d166378be
tomatoset <mtripilec@gmail.com>
2015-10-22 11:30:54 +0600

    Create MainActivity

---
+++ Global/app/src/main/java/com/tomatoset/app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.tomatoset.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import tomatoset.com.global.R;
+
+/**
+ * Created by barbarossa on 22.10.15.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
