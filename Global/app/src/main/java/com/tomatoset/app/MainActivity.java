9a68f65792ee22524b452543c3263f501adbf53f
macsim <mtripilec@gmail.com>
2015-10-23 01:19:39 +0600

    Create NavigationDrawer or NavigationView

--- Global/app/src/main/java/com/tomatoset/app/MainActivity.java
+++ Global/app/src/main/java/com/tomatoset/app/MainActivity.java
@@ -2,19 +2,21 @@ package com.tomatoset.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import tomatoset.com.global.R;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
