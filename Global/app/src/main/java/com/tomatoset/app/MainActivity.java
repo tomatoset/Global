e9b8681a839f84a279f19ec5c692146b573c3a75
tomatoset <mtripilec@gmail.com>
2015-10-22 15:10:14 +0600

    Create MainActivity

--- Global/app/src/main/java/com/tomatoset/app/MainActivity.java
+++ Global/app/src/main/java/com/tomatoset/app/MainActivity.java
@@ -2,16 +2,26 @@ package com.tomatoset.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import tomatoset.com.global.R;
 
-/**
- * Created by barbarossa on 22.10.15.
- */
+
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
     }
 }
