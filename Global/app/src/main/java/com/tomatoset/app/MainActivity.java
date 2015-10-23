9183e431dbe4092168949a8242862cee66b4dea8
tomatoset <mtripilec@gmail.com>
2015-10-23 13:22:43 +0600

    Add item to NavigationView

--- Global/app/src/main/java/com/tomatoset/app/MainActivity.java
+++ Global/app/src/main/java/com/tomatoset/app/MainActivity.java
@@ -1,7 +1,7 @@
 package com.tomatoset.app;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -11,17 +11,21 @@ import tomatoset.com.global.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT =R.layout.main_layout;
     private Toolbar toolbar;
-
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
     }
+
+
 
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -33,5 +37,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
     }
 }
