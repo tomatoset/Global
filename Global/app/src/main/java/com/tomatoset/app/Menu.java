96ea71a170e878d09b3927383ffee65f0ea9c0c5
tomatoset <mtripilec@gmail.com>
2015-11-24 18:26:09 +0600

    Add layout card

---
+++ Global/app/src/main/java/com/tomatoset/app/Menu.java
@@ -0,0 +1,14 @@
+package com.tomatoset.app;
+
+
+public class Menu {
+    String name;
+    String sum;
+    int photoId;
+
+    Menu(String name, String sum, int photoId) {
+        this.name = name;
+        this.sum = sum;
+        this.photoId = photoId;
+    }
+}
